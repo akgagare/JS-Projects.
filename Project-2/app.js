4664d2eb5dd519c0358204f1052a06a863bbcd0f
Arun <arungagare2915@gmail.com>
2025-06-11 20:03:36 +0530

    DOM Concept Revise Project

--- Project-2/app.js
+++ Project-2/app.js
@@ -5,12 +5,11 @@ let youScore = document.querySelector("#user-score");
 let compScore = document.querySelector("#comp-score");
 let msg2 = document.querySelector("#msg");
 
-// 1: Rock, 2: Paper, 3: Scissors
 function callComp() {
     return Math.floor(Math.random() * 3) + 1;
 }
 
-// Helper to display result message with styles
+
 function showMessage(text, color) {
     msg2.innerHTML = text;
     msg2.style.color = color;
@@ -22,18 +21,18 @@ function showMessage(text, color) {
     msg2.style.textAlign = "center";
 }
 
-// Game logic
+
 function play(userChoice) {
     const compChoice = callComp();
 
-    // 1: Rock, 2: Paper, 3: Scissors
+    
     if (userChoice === compChoice) {
         showMessage("It's a Draw!", "gray");
         console.log("draw");
     } else if (
-        (userChoice === 1 && compChoice === 3) ||  // Rock beats Scissors
-        (userChoice === 2 && compChoice === 1) ||  // Paper beats Rock
-        (userChoice === 3 && compChoice === 2)     // Scissors beats Paper
+        (userChoice === 1 && compChoice === 3) ||  
+        (userChoice === 2 && compChoice === 1) ||  
+        (userChoice === 3 && compChoice === 2)    
     ) {
         youScore.innerHTML = Number(youScore.innerHTML) + 1;
         showMessage("You have Won!", "green");
@@ -45,7 +44,7 @@ function play(userChoice) {
     }
 }
 
-// Event listeners
+
 rock.addEventListener("click", () => play(1));
 paper.addEventListener("click", () => play(2));
 scissors.addEventListener("click", () => play(3));
